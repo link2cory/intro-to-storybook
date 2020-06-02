e77639a027fca17e2606d229b1c2f2c0b898f3c2
Cory Perkins <link2cory@gmail.com>
2020-06-02 14:52:26 -0700

    added actions and link addon and added css reset into storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,4 +1,8 @@
 module.exports = {
   stories: ['../src/**/*.stories.[tjs]sx'],
-  addons: ['@storybook/preset-typescript'],
+  addons: [
+    '@storybook/preset-typescript',
+    '@storybook/addon-actions',
+    '@storybook/addon-links',
+  ],
 }
