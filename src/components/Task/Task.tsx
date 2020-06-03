73859244746dc18ebc5a2f4bc0271c93b56e274e
Cory Perkins <link2cory@gmail.com>
2020-06-02 17:23:13 -0700

    added some more details to the Task component

--- src/components/Task/Task.tsx
+++ src/components/Task/Task.tsx
@@ -10,16 +10,26 @@ interface TaskProps {
   task: {
     id: string,
     title: string,
+    state: TaskStates,
   };
-  state: TaskStates;
 }
 
 const Task: React.FunctionComponent<TaskProps> = ({
-  task: { id, title },
-  state,
+  task: { id, title, state },
 }: TaskProps) => (
-  <div className='list-item'>
-    <input type='text' value={title} readOnly />
+  <div className={`list-item ${state}`}>
+    <label className='checkbox'>
+      <input
+        type='checkbox'
+        defaultChecked={state === TaskStates.TASK_ARCHIVED}
+        disabled
+        name='checked'
+      />
+      <span className='checkbox-custom' />
+    </label>
+    <div className='title'>
+      <input type='text' value={title} readOnly placeholder='Input title' />
+    </div>
   </div>
 )
 
