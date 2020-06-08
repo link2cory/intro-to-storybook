517c5ca19d41926a2d20da332518f8cf3b502a0c
Cory Perkins <link2cory@gmail.com>
2020-06-07 19:13:07 -0700

    TaskList: added no tasks view

--- src/components/TaskList/TaskList.tsx
+++ src/components/TaskList/TaskList.tsx
@@ -39,8 +39,17 @@ const TaskList: React.FunctionComponent<TaskListProps> = ({
   }
 
   if (tasks.length === 0) {
-    return <div className='list-items'>empty</div>
+    return (
+      <div className='list-items'>
+        <div className='wrapper-message'>
+          <span className='icon-check' />
+          <div className='title-message'>You have no tasks</div>
+          <div className='subtitle-message'>Sit back and relax</div>
+        </div>
+      </div>
+    )
   }
+
   return (
     <div className='list-item'>
       {tasks.map((task) => (
